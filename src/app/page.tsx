826eed8f334d7dd51d372036cca84e7c63078d13
Perpy-del <perpetualmeninwa@gmail.com>
2023-12-27 04:09:51 +0100

    landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,12 @@ import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <Button>Click Me!</Button>
+    <div className="bg-gradient-to-r min-h-screen from-rose-100 to-teal-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <h1 className="font-semibold text-7xl text-center">AI <span className="text-green-600 font-bold">note taking</span> assistant</h1>
+        <div className="mt-4"></div>
+        <h2 className="font-semibold text-3xl text-center text-slate-700">AI Powered</h2>
+      </div>
+    </div>
   )
 }
