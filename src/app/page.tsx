5b5727d96264090022810ac171b43b3f6d7946e7
Perpy-del <perpetualmeninwa@gmail.com>
2023-12-27 22:57:46 +0100

    database setup with postgres

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,29 @@
-import { Button } from "@/components/ui/button"
+import TypewriterTitle from '@/components/TypewriterTitle';
+import { Button } from '@/components/ui/button';
+import { ArrowRight } from 'lucide-react';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className="bg-gradient-to-r min-h-screen from-rose-100 to-teal-100">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <h1 className="font-semibold text-7xl text-center">AI <span className="text-green-600 font-bold">note taking</span> assistant</h1>
+        <h1 className="font-semibold text-7xl text-center">
+          AI <span className="text-green-600 font-bold">note taking</span>{' '}
+          assistant
+        </h1>
         <div className="mt-4"></div>
-        <h2 className="font-semibold text-3xl text-center text-slate-700">AI Powered</h2>
+        <h2 className="font-semibold text-3xl text-center text-slate-700">
+          <TypewriterTitle />
+        </h2>
+        <div className="mt-8"></div>
+        <div className="flex justify-center">
+          <Link href="/dashboard">
+            <Button className="bg-green-600">
+              Get Started <ArrowRight className='ml-2 w-s h-5' />
+            </Button>
+          </Link>
+        </div>
       </div>
     </div>
-  )
+  );
 }
